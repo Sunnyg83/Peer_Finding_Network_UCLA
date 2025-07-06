bc84951a9bb309a6a44a6afb1d0fbea49d129155
Sunny <sgandhari06@gmail.com>
2025-07-05 19:19:40 -0500

    Connect user routes to Express server

--- server/index.js
+++ server/index.js
@@ -22,10 +22,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// import user routes
+const userRoutes = require('./routes/userRoutes');
+
 // test route
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend is working!' });
 });
+
+// use user routes
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`API available at: http://localhost:${PORT}/api/test`);
