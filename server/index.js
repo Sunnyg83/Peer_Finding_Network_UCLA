180e080bda21a5323757a9ef0921efc7e18ee23b
Sunny <sgandhari06@gmail.com>
2025-07-10 00:17:08 -0500

    Added vercel to origin

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ const corsOptions = {
   origin: [
     'http://127.0.0.1:5173', 
     'http://localhost:5173',
-    'https://your-frontend-domain.vercel.app', // Add your Vercel domain here
-    'https://ucla-study-network.vercel.app' // Example domain
+    'https://peer-finding-network-ucla.vercel.app', 
+   
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
